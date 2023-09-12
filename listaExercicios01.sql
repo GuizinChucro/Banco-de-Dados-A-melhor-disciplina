8ac011525a16f3e7695c9b4c2d9716f4d19a920d
Guizinduarrancadiu <115833117+GuizinChucro@users.noreply.github.com>
2023-09-12 10:11:00 -0300

    Exercício 3

--- listaExercicios01.sql
+++ listaExercicios01.sql
@@ -2,3 +2,7 @@ SELECT titulo FROM livros;
 
 SELECT nome FROM autores
 WHERE YEAR(nascimento) < 1900;
+
+SELECT livros.titulo FROM livros
+INNER JOIN autores ON livros.autor_id = autores.id
+WHERE autores.nome = 'J.K. Rowling';
