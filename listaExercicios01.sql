19fb3570d7dd4fa6e662b7734cba8e05c53317d2
Guizinduarrancadiu <115833117+GuizinChucro@users.noreply.github.com>
2023-09-12 10:13:57 -0300

    Exercício 8

--- listaExercicios01.sql
+++ listaExercicios01.sql
@@ -22,3 +22,5 @@ GROUP BY autores.nome;
 
 SELECT matriculas.curso, COUNT(alunos.id) as numero_de_alunos FROM matriculas
 INNER JOIN alunos ON matriculas.aluno_id = alunos.id GROUP BY matriculas.curso;
+
+SELECT produto, AVG(receita) as media_de_receita FROM vendas GROUP BY produto;
