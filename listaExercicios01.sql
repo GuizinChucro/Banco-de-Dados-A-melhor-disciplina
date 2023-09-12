988c254fba132fbe7c870fdc866af2e0e366593d
Guizinduarrancadiu <115833117+GuizinChucro@users.noreply.github.com>
2023-09-12 10:15:05 -0300

    Exercício 9

--- listaExercicios01.sql
+++ listaExercicios01.sql
@@ -24,3 +24,6 @@ SELECT matriculas.curso, COUNT(alunos.id) as numero_de_alunos FROM matriculas
 INNER JOIN alunos ON matriculas.aluno_id = alunos.id GROUP BY matriculas.curso;
 
 SELECT produto, AVG(receita) as media_de_receita FROM vendas GROUP BY produto;
+
+SELECT produto, SUM(receita) as receita_total FROM vendas
+GROUP BY produto HAVING SUM(receita) > 10000;
