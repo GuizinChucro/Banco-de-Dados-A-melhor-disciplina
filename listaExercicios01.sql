e995f626c2f6e71cd4e22bff2c7f128e75ac2d78
Guizinduarrancadiu <115833117+GuizinChucro@users.noreply.github.com>
2023-09-12 10:11:21 -0300

    Exercício 4

--- listaExercicios01.sql
+++ listaExercicios01.sql
@@ -6,3 +6,7 @@ WHERE YEAR(nascimento) < 1900;
 SELECT livros.titulo FROM livros
 INNER JOIN autores ON livros.autor_id = autores.id
 WHERE autores.nome = 'J.K. Rowling';
+
+SELECT alunos.nome FROM alunos
+INNER JOIN matriculas ON alunos.id = matriculas.aluno_id
+WHERE matriculas.curso = 'Engenharia de Software';
